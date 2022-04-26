8d7736816b8a48bd024ae0395de592fc0bb44fc3
Paula Gomez Ariza <pgomezariza@gmail.com>
2022-04-26 09:33:10 +0200

    add header

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,43 @@
-import '../styles/App.scss';
+// 1. PINTAR un LISTADO 
+// 2. AÑADIR NUEVA FRASE 
 
-function App() {}
+import '../styles/App.scss';
+import quoteList from '../data/quote.json';
+import { useState } from 'react';
+
+
+function App() {
+  
+  //VARIABLES DE ESTADO 
+  const [data, setData] = useState(quoteList);
+
+  return (
+    <div className="page">
+      {/* header */}
+      <header className="header">
+        <h1 className="header__title">Frases de Friends</h1>
+        <form>
+          <input
+            className="header__search"
+            autoComplete="off"
+            type="search"
+            name="search"
+            placeholder="Filtrar listado frases"
+            //onChange={handleSearch}
+            //value={search}
+          />
+        </form>
+      </header>
+      <main>
+
+      </main>
+      {/* quote list */}
+      <ul className="quote__list">{htmlData}</ul>
+
+    </div>
+  );
+  
+}
+
 
 export default App;
